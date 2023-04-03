212cb4679d17f4c913aedd4548d65b8de7ca0fa4
Ryan Current <rjc7379@g.rit.edu>
2023-04-03 14:07:00 -0400

    Application templates added

---
+++ src/serialization/SerializableObject.cs
@@ -0,0 +1,11 @@
+﻿namespace serialization;
+/// <summary>
+/// This class will be used to create objects that will be serialized. This is 
+/// supposed to represent a typical class. 
+/// </summary>
+public class SerialzableObject
+{
+    public int IntegerAttribute { get; set; } = 0; 
+    public string StringAttribute { get; set; } = "This is an example string that must be serialized"; 
+    public bool BoolAttribute { get; set; } = false; 
+}
